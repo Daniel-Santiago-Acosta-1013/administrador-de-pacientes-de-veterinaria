a67ddf0491eea7c5a8cf1508fa13808d1f740e88
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2021-05-28 12:40:13 -0500

    Mostrar citas en el HTML

--- js/app.js
+++ js/app.js
@@ -14,6 +14,12 @@ class Citas {
     constructor() {
         this.citas = [];
     }
+
+    agregarCita(cita) {
+        this.citas = [...this.citas, cita];
+
+        // console.log(this.citas);
+    }
 }
 
 class UI {
@@ -40,6 +46,68 @@ class UI {
         setTimeout(() => {
             divMensaje.remove();
         }, 5000);
+    }
+
+    imprimirCitas({citas}) {
+
+        this.limpiarHTML();
+
+        citas.forEach(cita => {
+            const { mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3');
+            divCita.dataset.id = id;
+
+            // Scripting de los elementos de la cita 
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+            mascotaParrafo.textContent = mascota;
+
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML = `
+                <span class="font-weight-bolder">Propietario: </span> ${propietario}
+            `;
+
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `
+                <span class="font-weight-bolder">Telefono: </span> ${telefono}
+            `;
+
+            const fechaParrafo = document.createElement('p');
+            fechaParrafo.innerHTML = `
+                <span class="font-weight-bolder">Fecha: </span> ${fecha}
+            `;
+
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML = `
+                <span class="font-weight-bolder">Hora: </span> ${hora}
+            `;
+
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML = `
+                <span class="font-weight-bolder">Sintomas: </span> ${sintomas}
+            `;
+            
+
+            // Agregar los parrafos al divCita
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+            
+
+            // Agregar citas al HTML 
+            contenedorCitas.appendChild(divCita);
+        });
+    }
+
+    limpiarHTML() {
+        while(contenedorCitas.firstChild) {
+            contenedorCitas.removeChild( contenedorCitas.firstChild );
+        }
     }
 
 }
@@ -88,5 +156,27 @@ function nuevaCita(e) {
         return;
     }
 
+    // Generar un id unico 
+    citaObj.id = Date.now();
+
     // Crear una nueva cita
+    administrarCitas.agregarCita({...citaObj});
+
+    // Reiniciar el objeto para la validacion 
+    reiniciarObjeto();
+
+    //reiniciar el formulario
+    formulario.reset();
+
+    // Mostrar el HTML de las citas
+     ui.imprimirCitas(administrarCitas);
+}
+
+function reiniciarObjeto() {
+    citaObj.mascota = '';
+    citaObj.propietario = '';
+    citaObj.telefono = '';
+    citaObj.fecha = '';
+    citaObj.hora = '';
+    citaObj.sintomas = '';
 }
