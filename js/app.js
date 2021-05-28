631dffb713d7d51decb74fcdaf45acb00e6a0bc8
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2021-05-28 18:24:17 -0500

    quitando cb

--- js/app.js
+++ js/app.js
@@ -94,6 +94,9 @@ class UI {
             btnEliminar.classList.add('btn', 'btn-danger', 'mr-2')
             btnEliminar.innerHTML = 'Eliminar <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>'; 
 
+            btnEliminar.onclick = () => eliminarCita(id);
+
+
             // Agregar los parrafos al divCita
             divCita.appendChild(mascotaParrafo);
             divCita.appendChild(propietarioParrafo);
@@ -184,4 +187,8 @@ function reiniciarObjeto() {
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+}
+
+function eliminarCita(id) {
+    console.log(id)
 }
