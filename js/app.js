01ce955316fed20dba98b0f5cb9e22bbcebf1168
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2021-06-01 16:11:41 -0500

    finalizacion de la base de datos basica con IndexedDB

--- js/app.js
+++ js/app.js
@@ -155,6 +155,7 @@ class UI {
     
                 // Añade un botón de editar...
                 const btnEditar = document.createElement('button');
+                const cita = cursor.value;
                 btnEditar.onclick = () => cargarEdicion(cita);
     
                 btnEditar.classList.add('btn', 'btn-info');
@@ -214,11 +215,24 @@ function nuevaCita(e) {
         // Estamos editando
         administrarCitas.editarCita( {...citaObj} );
 
-        ui.imprimirAlerta('Guardado Correctamente');
+        // Edita el indexdDB
+        const transaction = DB.transaction(['citas'], 'readwrite')
+        const objectStore = transaction.objectStore('citas')
 
-        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
+        objectStore.put(citaObj);
 
-        editando = false;
+        transaction.onecomplete = () => {
+
+            ui.imprimirAlerta('Guardado Correctamente');
+
+            formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
+    
+            editando = false;
+        }
+
+        transaction.onerror = () => {
+            console.log('Hubo un error');
+        }
 
     } else {
         // Nuevo Registro
@@ -270,9 +284,20 @@ function reiniciarObjeto() {
 
 
 function eliminarCita(id) {
-    administrarCitas.eliminarCita(id);
+    
+    const transaction = DB.transaction('citas', 'readwrite');
+    const objectStore = transaction.objectStore('citas');
 
-    ui.imprimirCitas()
+    objectStore.delete(id);
+
+    transaction.oncomplete = () => {
+        console.log(`Cita ${id} eliminada...`);
+        ui.imprimirCitas();
+    }
+
+    transaction.onerror = () => {
+        console.log('Hubo un error');
+    }
 }
 
 function cargarEdicion(cita) {
